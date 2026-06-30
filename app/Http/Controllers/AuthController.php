f8bc56f730517b79e26ac02be50158402e513d43
madebynoxc <glebx95@gmail.com>
2026-06-29 22:10:59 -0400

    Reduce the scope required for auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
      */
     public function redirect()
     {
-        return Socialite::driver('discord')->redirect();
+        return Socialite::driver('discord')->setScopes(['identify'])->redirect();
     }
 
     /**
